594cbf6790133ebb6c966972689ff3e1f1dee642
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2023-12-19 14:22:21 +0900

    commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ *  #3-1-1. localhost:3000/dashboard 경로로 접근할 수 있는 라우팅 컴포넌트
+ *  page라는 이름을 사용할 경우 페이지의 구성 요소로 포함될 UI 컴포넌트 및 테스트 파일, 다른 관련 있는 라우팅 등을 배치할 수 있게 제공
+ */
+
+const Page = () => {
+  return (
+    <div>Dashborad Page</div>
+  )
+}
+
+export default Page
